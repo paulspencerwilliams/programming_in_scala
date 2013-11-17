ce37e4f64c986021cae3313c96450e1fa1dce1f3
Paul Williams <paulswilliams@me.com>
2013-11-17 21:22:52 +0000

    added + and x operators

--- rational/Rational.scala
+++ rational/Rational.scala
@@ -1,10 +1,22 @@
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  val numer: Int = n
-  var denom: Int = d
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
 
   def this(n:Int) = this(n, 1)
-  override def toString = n +"/"+ d
+  
+  def + (that: Rational): Rational =
+    new Rational (
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  def * (that: Rational): Rational = 
+    new Rational(numer * that.numer, denom * that.denom)
+
+
+  override def toString =  numer +"/"+ denom
 
   def add(that: Rational): Rational =
     new Rational(n * that.denom + that.numer * d, d * that.denom)
@@ -14,5 +26,8 @@ class Rational(n: Int, d: Int) {
   
   def max(that: Rational) =
     if (this.lessThan(that)) that else this
+  
+  private def gcd(a: Int, b:Int): Int = 
+    if (b == 0) a else gcd(b, a % b)
 }
 
