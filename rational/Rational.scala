7f8ea03cef5c9054394c08dbbefcd2137d335333
Paul Williams <paulswilliams@me.com>
2013-11-17 20:14:56 +0000

    upto 6.2 - prior normalising rational

--- rational/Rational.scala
+++ rational/Rational.scala
@@ -2,9 +2,17 @@ class Rational(n: Int, d: Int) {
   require(d != 0)
   val numer: Int = n
   var denom: Int = d
+
+  def this(n:Int) = this(n, 1)
   override def toString = n +"/"+ d
 
   def add(that: Rational): Rational =
     new Rational(n * that.denom + that.numer * d, d * that.denom)
+  
+  def lessThan(that: Rational)  =
+    this.numer * that.denom < that.numer * this.denom
+  
+  def max(that: Rational) =
+    if (this.lessThan(that)) that else this
 }
 
