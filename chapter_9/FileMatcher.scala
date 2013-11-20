74999710dcab4d6a0216975989f9321b2fee36b7
Paul Williams <paulswilliams@me.com>
2013-11-20 18:24:14 +0000

    further DRYed through removing function value parameter

--- chapter_9/FileMatcher.scala
+++ chapter_9/FileMatcher.scala
@@ -2,16 +2,16 @@ object FileMatcher {
   private def filesHere = (new java.io.File("../.")).listFiles
 
   def filesEnding(query: String) =
-    filesMatching(query, _.endsWith(_))
+    filesMatching(_.endsWith(query))
 
   def filesContaining(query: String) =
-    filesMatching(query, _.contains(_))
+    filesMatching(_.contains(query))
 
   def filesRegex(query: String) =
-    filesMatching(query, _.matches(_))
+    filesMatching(_.matches(query))
 
-  def filesMatching(query: String, matcher: (String, String) => Boolean) = {
-    for (file <- filesHere; if matcher(file.getName, query))
+  def filesMatching(matcher: String => Boolean) = {
+    for (file <- filesHere; if matcher(file.getName))
       yield file
   }
 }
