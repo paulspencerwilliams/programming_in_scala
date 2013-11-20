591db562997b9b085cd08d912c73bf911cd4ebf7
Paul Williams <paulswilliams@me.com>
2013-11-20 18:07:18 +0000

    implemented undry filesContaining

--- chapter_9/FileMatcher.scala
+++ chapter_9/FileMatcher.scala
@@ -4,6 +4,11 @@ object FileMatcher {
   def filesEnding(query: String) =
     for (file <- filesHere; if file.getName.endsWith(query))
       yield file
+
+
+  def filesContaining(query: String) =
+    for (file <- filesHere; if file.getName.contains(query))
+      yield file
 }
 
 
