26b50bcf7524f8dcb7e930f3b445890f1a966336
Paul Williams <paulswilliams@me.com>
2013-11-22 07:57:52 +0000

    printing to file as custom control abstraction

---
+++ chapter_9/MyControlPatterns.scala
@@ -0,0 +1,10 @@
+import java.io._
+
+def withPrintWriter(file: File, op: PrintWriter => Unit) {
+  val writer = new PrintWriter(file)
+  try {
+    op(writer)
+  } finally {
+    writer.close()
+  }
+}
