a250a820bf0a81ac0b0027113e0930b3e31b05e6
Paul Williams <paulswilliams@me.com>
2013-11-23 15:00:42 +0000

    finished chapter 9

--- chapter_9/MyAsserts.scala
+++ chapter_9/MyAsserts.scala
@@ -3,3 +3,7 @@ var assertionsEnabled = true
 def myAssert(predicate: () => Boolean) =
   if (assertionsEnabled && !predicate())
     throw new AssertionError
+
+def byNameAssert(predicate: => Boolean) = 
+  if (assertionsEnabled && !predicate)
+    throw new AssertionError
