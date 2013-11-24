ff5adb92cf43477fe2ccfc50fdb336f121423e6a
Paul Williams <paulswilliams@me.com>
2013-11-24 14:31:11 +0000

    ready to try the spiral app

--- chapter_10/Element.scala
+++ chapter_10/Element.scala
@@ -1,4 +1,13 @@
 object Element {
+
+  def elem(contents: Array[String]): Element = new ArrayElement(contents)
+
+  def elem(chr: Char, width: Int, height: Int): Element = 
+    new UniformElement(chr, width, height)
+
+  def elem(line: String): Element =
+    new LineElement(line)
+
   abstract class Element {
     def contents: Array[String]
 
@@ -6,28 +15,61 @@ object Element {
       if (height == 0) 0 else contents(0).length
 
     def height: Int = contents.length
-     
-    def above(that: Element): Element = 
-      new ArrayElement(this.contents ++ that.contents)
 
-    def beside(that: Element): Element = 
-      new ArrayElement(
+    def above(that: Element): Element = {
+      val this1 = this widen that.width
+      val that1 = that widen this.width
+      elem(this1.contents ++ that1.contents)
+    }
+
+    def beside(that: Element): Element = {
+      val this1 = this heighten that.height
+    val that1 = that heighten this.height
+      elem(
         for (
-             (line1, line2) <- this.contents zip that.contents
+             (line1, line2) <- this1.contents zip that1.contents
            ) yield line1 + line2
          )
+    }
 
-    override def toString = contents mkString "\n"
+    def widen(w: Int): Element =
+      if (w <= width) this
+      else {
+        val left = elem(' ', (w - width) / 2, height)
+        var right = elem(' ', w - width - left.width, height)
+        left beside this beside right
+      }
+    
+
+    def heighten(h: Int): Element = 
+      if (h <= height) this
+      else {
+        val top = elem(' ', width, (h - height) / 2)
+        val bot = elem(' ', width, h - height - top.height)
+        top above this above bot 
+      }
+
+
+      override def toString = contents mkString "\n"
+      }
+
+      private class ArrayElement(conts: Array[String]) extends Element  {
+        def contents: Array[String] = conts
   }
 
-  class ArrayElement(conts: Array[String]) extends Element  {
-    def contents: Array[String] = conts
-  }
-
-  class LineElement(s:String) extends Element() {
+  private class LineElement(s:String) extends Element() {
     val contents = Array(s)
     override def width = s.length
     override def height = 1
+      }
+
+      private class UniformElement(
+        ch: Char,
+        override val width: Int, 
+        override val height: Int
+      ) extends Element {
+        private val line = ch.toString * width
+        def contents = Array.fill(height)(line)
   }
 
-  }
+      }
