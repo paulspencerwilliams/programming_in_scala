e165d9220b469bf05e5b13f48e13501cfa1585a9
Paul Williams <paulswilliams@me.com>
2013-11-27 08:22:06 +0000

    added chapter 12 traits

---
+++ chapter_12/Rational.scala
@@ -0,0 +1,56 @@
+class Rational(n: Int, d: Int) extends Ordered[Rational] {
+  require(d != 0)
+  private val g = gcd(n.abs, d.abs)
+  val numer = n / g
+  val denom = d / g
+
+  def this(n:Int) = this(n, 1)
+  
+  def compare(that: Rational) =
+    (this.numer * that.denom) - (that.numer * that.denom)
+  
+  def + (that: Rational): Rational =
+    new Rational (
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+
+  def + (i: Int): Rational =
+    new Rational(numer + i * denom, denom)
+
+  def - (that: Rational): Rational =
+    new Rational(
+      numer * that.denom - that.numer * denom,
+      denom * that.denom
+    )
+
+  def - (i: Int): Rational =
+    new Rational(numer -i * denom, denom)
+  
+  
+  def * (that: Rational): Rational = 
+    new Rational(numer * that.numer, denom * that.denom)
+
+  def * (i: Int): Rational = 
+    new Rational(numer * i, denom)
+
+  def / (that: Rational): Rational =
+    new Rational(numer * that.denom, denom * that.numer)
+
+  def / (i: Int): Rational =
+    new Rational(numer, denom * i)
+  override def toString =  numer +"/"+ denom
+
+  def add(that: Rational): Rational =
+    new Rational(n * that.denom + that.numer * d, d * that.denom)
+  
+  def lessThan(that: Rational)  =
+    this.numer * that.denom < that.numer * this.denom
+  
+  def max(that: Rational) =
+    if (this.lessThan(that)) that else this
+  
+  private def gcd(a: Int, b:Int): Int = 
+    if (b == 0) a else gcd(b, a % b)
+}
+
