ed1d5dc44dac6fd6b7f1795cdfbc3364030c0692
Paul Williams <paulswilliams@me.com>
2013-12-22 16:12:21 +0000

    translated to use List pattern matching

--- chapter_16/Lists.scala
+++ chapter_16/Lists.scala
@@ -1,13 +1,12 @@
 object InsertionSort1 {
-  def isort(xs: List[Int]): List[Int] =
-    if (xs.isEmpty) Nil
-    else insert(xs.head, isort(xs.tail))
+  def isort(xs: List[Int]): List[Int] = xs match {
+    case List() => List()
+    case x :: xs1 => insert(x, isort(xs1))
+  }
 
-    def insert(x: Int, xs: List[Int]): List[Int] = 
-      if (xs.isEmpty || x <= xs.head) x :: xs
-      else xs.head :: insert(x, xs.tail)
-
-      def main(args: Array[String]) {
-        println("isort(List(5, 3, 12)) [" + isort(List(5, 3, 12)) + "]")
-      }
-      }
+  def insert(x: Int, xs: List[Int]): List[Int] = xs match { 
+    case List() => List(x)
+    case y :: ys => if (x <= y) x :: xs
+    else y :: insert(x, ys)
+  }
+}  
